e282642a16ec19440e1f668300197659194a912f
aniruddha <aniruddha.kudalkar@gmail.com>
2017-03-22 12:18:58 +0530

    storage : sqlite CRUD operations

---
+++ sqlite/src/main/java/com/codekul/sqlite/Db.java
@@ -0,0 +1,12 @@
+package com.codekul.sqlite;
+
+/**
+ * Created by aniruddha on 22/3/17.
+ */
+
+public interface Db {
+    String DB_NAME ="products.db";
+    String COL_PROD_NAME = "product_name";
+    String COL_PROD_COST = "product_cost";
+    String TAB_PRODS = "products";
+}
