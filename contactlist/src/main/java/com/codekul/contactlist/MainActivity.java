20e88c2b4957ea5873428b923ac2a640527c0427
aniruddha <aniruddha.kudalkar@gmail.com>
2017-03-23 12:26:58 +0530

    runtime permission : getting runtime permission for reading contacts

--- contactlist/src/main/java/com/codekul/contactlist/MainActivity.java
+++ contactlist/src/main/java/com/codekul/contactlist/MainActivity.java
@@ -1,9 +1,16 @@
 package com.codekul.contactlist;
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -13,11 +20,41 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQ_CODE_PER = 1236;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadMyData();
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+
+                new AlertDialog.Builder(this)
+                        .setTitle("Request")
+                        .setMessage("Dont worry we will not steal your data :)")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.READ_CONTACTS},
+                                        REQ_CODE_PER);
+                            }
+                        }).create().show();
+
+            } else {
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        REQ_CODE_PER);
+            }
+        } else {
+            loadContacts();
+        }
     }
 
     private void loadContacts() {
@@ -63,5 +100,17 @@ public class MainActivity extends AppCompatActivity {
 
         ((ListView) findViewById(R.id.listContacts))
                 .setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dataSet));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQ_CODE_PER) {
+            if(grantResults.length > 0 ) {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    loadContacts();
+                }
+            }
+        }
     }
 }
